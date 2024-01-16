7b623e7e4ee5820e75c962527a602bca71dc1d5f
Nico Stöhr <58736331+nicostoehr@users.noreply.github.com>
2024-01-17 00:03:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function toDisplay(key) {
     if (display.value == "Error") {
         display.value = key;
     }
-    else {
+    else if (key == "x" && display.value.length > 0) {
+        display.value = display.value.slice(0, -1);
+    }
+    else if (key != "x") {
         display.value += key;
     }
 }
